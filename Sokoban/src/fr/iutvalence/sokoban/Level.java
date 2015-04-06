3ff89d36c8740a3a950d5b6ddd26dd05d6c841cd
TZK <thib-17@orange.fr>
2015-04-06 19:45:23 +0200

    Ajout de la méthode loadFromFile (à compléter)     - Permet de charger un niveau dynamiquement à partir d'un fichier

--- Sokoban/src/fr/iutvalence/sokoban/Level.java
+++ Sokoban/src/fr/iutvalence/sokoban/Level.java
@@ -1,5 +1,10 @@
 package fr.iutvalence.sokoban;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import fr.iutvalence.sokoban.exception.InvalidMapPositionException;
 
 
@@ -51,6 +56,79 @@ public class Level {
 		}
 		
 		this.map[position.getX()][position.getY()] = cell;
+	}
+	
+	/**
+	 * Charge un niveau à partir d'un fichier
+	 * @param filePath
+	 */
+	public void loadFromFile(String filePath){
+		try {
+			BufferedReader buffer = new BufferedReader(new FileReader(filePath));
+			
+			// Ligne courrante
+			String currentLine;
+			
+			// Contient l'ensemble des lignes
+			StringBuilder allLines = new StringBuilder();
+			
+			/* On parcours le fichier:
+			 *   - Récupère le nombre de lignes
+			 *   - Récupère le nombre max de caractères par lignes
+			 *   - Stocke le contunu du fichier dans une variable
+			 */
+			while((currentLine = buffer.readLine()) != null){
+				this.mapHeight++;
+				if(this.mapWidth < currentLine.length())
+					this.mapWidth++;
+				allLines.append(currentLine);
+				allLines.append('\n');
+			}
+			
+			// On initialise le tableau this.map
+			this.map = new CellCharacter[this.mapHeight][this.mapWidth];
+			
+			// On remplit le tableau map
+			for (int line = 0; line < this.mapHeight; line++) {
+				for (int column = 0; column < this.mapWidth; column++) {
+					char currentChar = allLines.toString().charAt(line * 10 + column);
+					Position pos = new Position(line, column);
+					switch (currentChar) {
+						case '#':
+							this.addCell(CellCharacter.WALL, pos);
+							break;
+						case ' ':
+							this.addCell(CellCharacter.FLOOR, pos);
+							break;
+						case '.':
+							this.addCell(CellCharacter.TARGET, pos);
+							break;
+						case '@':
+							this.addCell(CellCharacter.PLAYER, pos);
+							break;
+						case '+':
+							this.addCell(CellCharacter.PLAYER_ON_TARGET, pos);
+							break;
+						case '$':
+							this.addCell(CellCharacter.BOX, pos);
+							break;
+						case '*':
+							this.addCell(CellCharacter.BOX_ON_TARGET, pos);
+							break;
+						default:
+							this.addCell(CellCharacter.FLOOR, pos);
+							break;
+					}
+				}
+			}		
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InvalidMapPositionException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
