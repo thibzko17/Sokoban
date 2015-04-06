21c94ee0fb0c1d8d4661d636a8731c2cacb39317
TZK <thib-17@orange.fr>
2015-04-06 19:44:37 +0200

    Ajout de la classe InvalidMapPositionException      - Gère les exceptions lors d'un appel à une cellule non valide

---
+++ Sokoban/src/fr/iutvalence/sokoban/exception/InvalidMapPositionException.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.sokoban.exception;
+
+/**
+ * Exception générée lors d'un appel a une case
+ *     du plateau non valide
+ * @author Thibault
+ * @version 1.0
+ */
+public class InvalidMapPositionException extends Exception{
+
+	private static final long serialVersionUID = -519370135081726794L;
+
+}
